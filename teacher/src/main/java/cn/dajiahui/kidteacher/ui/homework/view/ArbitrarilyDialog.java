ed4c5eceeda8b090a3f4dc9f87ec252915a0e766
majin5555 <ma.jin@dajiahui.cn>
2018-03-12 12:22:41 +0800

    修改作业模块中班级选择和状态选择的UI

---
+++ teacher/src/main/java/cn/dajiahui/kidteacher/ui/homework/view/ArbitrarilyDialog.java
@@ -0,0 +1,63 @@
+package cn.dajiahui.kidteacher.ui.homework.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.fxtx.framework.R;
+
+/**
+ * 任意位置的dialog
+ */
+public abstract class ArbitrarilyDialog extends Dialog {
+    protected Context mContext;
+    protected View rootView;
+    int mDialogHeight;
+
+    public ArbitrarilyDialog(Context c, int resId, int mDialogHeight) {
+        super(c, R.style.transparentDialogChoiseClass);//
+        this.mContext = c;
+        this.mDialogHeight = mDialogHeight;
+        rootView = LayoutInflater.from(mContext).inflate(resId, null);
+        LinearLayout rootView = (LinearLayout) this.rootView;
+       /*设置透明度 全透明*/
+        rootView.getBackground().setAlpha(130);
+        initView();
+        this.setContentView(this.rootView);
+        initPop(resId);
+        /*关闭弹框*/
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    /**
+     * 初始化
+     */
+    private void initPop(int resId) {
+        Window window = this.getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.gravity = Gravity.BOTTOM;
+        lp.x = 0;//x作标
+        lp.height = mDialogHeight;//设置dialog的高度
+
+        // 设置显示位置
+        this.onWindowAttributesChanged(lp);
+        this.setCanceledOnTouchOutside(true);// 设置点击Dialog外部任意区域关闭Dialog
+        window.setAttributes(lp);
+        /*設置窗口透明*/
+        window.setDimAmount(0f);
+
+
+    }
+
+    public abstract void initView();
+}
---
+++ teacher/src/main/java/cn/dajiahui/kidteacher/ui/homework/view/NoScrollViewPager.java
@@ -0,0 +1,61 @@
+package cn.dajiahui.kidteacher.ui.homework.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by lenovo on 2018/1/25.
+ *
+ * 禁止滑动的ViewPager
+ */
+
+public class NoScrollViewPager extends ViewPager {
+
+    public boolean noScroll = true;//控制是否滑动
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        if (noScroll)
+            return false;
+        else
+            return super.onTouchEvent(arg0);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        if (noScroll)
+            return false;
+        else
+            return super.onInterceptTouchEvent(arg0);
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        super.setCurrentItem(item, smoothScroll);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        super.setCurrentItem(item);
+    }
+
+}
